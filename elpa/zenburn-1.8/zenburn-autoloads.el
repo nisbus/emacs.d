a7565cf6a1fe8ad15719dcb86f74b7defaf27728
nisbus <nisbus@gmail.com>
2011-07-26 11:41:07 +0000

    added haskell-mode to init.el

--- elpa/zenburn-1.8/zenburn-autoloads.el
+++
@@ -1,26 +0,0 @@
-;;; zenburn-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-
-
-;;;### (autoloads (color-theme-zenburn) "zenburn" "zenburn.el" (19940
-;;;;;;  11971))
-;;; Generated autoloads from zenburn.el
-
-(autoload (quote color-theme-zenburn) "zenburn" "\
-Just some alien fruit salad to keep you in the zone.
-
-\(fn)" t nil)
-
-;;;***
-
-;;;### (autoloads nil nil ("zenburn-pkg.el") (19940 11971 294662))
-
-;;;***
-
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; End:
-;;; zenburn-autoloads.el ends here
